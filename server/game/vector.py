532728e7fa4e935b42f9e5a6337dd3df4121f8a3
JackDesBwa <jackdesbwa@gmail.com>
2026-01-09 18:29:26 +0100

    Add hypervoxels_line helper

--- server/game/vector.py
+++ server/game/vector.py
@@ -1,3 +1,35 @@
+def hypervoxels_line(p0, p1, u=None):
+    p0 = list(p0)
+    p1 = list(p1)
+    dim = len(p0)
+
+    delta = [abs(p1[i] - p0[i]) for i in range(dim)]
+    step = [1 if p1[i] > p0[i] else -1 for i in range(dim)]
+    axis = max(range(dim), key=lambda i: delta[i])
+
+    errors = [delta[axis] // 2 for _ in range(dim)]
+    p = p0[:]
+
+    while True:
+        yield list(map(
+            lambda x, u: round(x) % u,
+            p,
+            u
+        )) if u else p[:]
+
+        if p == p1:
+            break
+
+        for i in range(dim):
+            if i == axis:
+                continue
+            errors[i] -= delta[i]
+            if errors[i] < 0:
+                p[i] += step[i]
+                errors[i] += delta[axis]
+        p[axis] += step[axis]
+
+
 class Vector:
     def __init__(self, u, pos):
         self.u = u
