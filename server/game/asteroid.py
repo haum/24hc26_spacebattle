26a623ed53f1d629da905f33816f30ea1adb73ce
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2026-03-04 00:01:34 +0100

    Remove unused asteroid destroy code

--- server/game/asteroid.py
+++ server/game/asteroid.py
@@ -4,8 +4,5 @@ class Asteroid:
         self.position = position
         self.u.add(self, ['asteroid', 'collidable'])
 
-    async def destroy(self):
-        self.u.remove(self)
-
     def __str__(self):
         return f'Asteroid(p={self.position})'
